9b726e9a7cbfc55049a72d0b4378b4d6e5f0c40c
craig <craig.durfey@intel.com>
2020-06-12 14:11:07 -0700

    Adding HTML files for API
    Signed-off-by: craig <craig.durfey@intel.com>

---
+++ html/search/variables_8.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['kd_5fcsum_5flen_661',['kd_csum_len',['../structdaos__key__desc__t.html#a67034737da6ffd41805782226bc3745b',1,'daos_key_desc_t']]],
+  ['kd_5fcsum_5ftype_662',['kd_csum_type',['../structdaos__key__desc__t.html#a0c2b7c65cd87c2ac99556752fed16790',1,'daos_key_desc_t']]],
+  ['kd_5fkey_5flen_663',['kd_key_len',['../structdaos__key__desc__t.html#abf51ea8a799cb106f76784e1f07e603b',1,'daos_key_desc_t']]],
+  ['kd_5fval_5ftype_664',['kd_val_type',['../structdaos__key__desc__t.html#a0045348784e873fe5b436dc5187f34e3',1,'daos_key_desc_t']]],
+  ['kds_665',['kds',['../structdaos__obj__list__t.html#ad18e6de859bab889577e42129c68217c',1,'daos_obj_list_t::kds()'],['../structdaos__kv__list__t.html#ad18e6de859bab889577e42129c68217c',1,'daos_kv_list_t::kds()']]],
+  ['key_666',['key',['../structdaos__kv__get__t.html#acd3d88da3c0e0313c3645ff34f62f542',1,'daos_kv_get_t::key()'],['../structdaos__kv__put__t.html#acd3d88da3c0e0313c3645ff34f62f542',1,'daos_kv_put_t::key()'],['../structdaos__kv__remove__t.html#acd3d88da3c0e0313c3645ff34f62f542',1,'daos_kv_remove_t::key()']]],
+  ['key_5fid_667',['key_id',['../structdaos__set__params__t.html#a3c9e1911e32476da4aa8b1951b3b634c',1,'daos_set_params_t']]]
+];
