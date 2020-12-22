0c02226e98f6606eea938c1366e8801711cd5d7b
cdurf1 <craig.durfey@intel.com>
2020-12-22 15:57:05 -0500

    Signed-off-by: craig <craig.durfey@intel.com> (#25)

--- html/structdaos__pool__connect__t.js
+++ html/structdaos__pool__connect__t.js
@@ -5,5 +5,5 @@ var structdaos__pool__connect__t =
     [ "info", "structdaos__pool__connect__t.html#ac8da9aadb56674709f5cf11ea3c24507", null ],
     [ "poh", "structdaos__pool__connect__t.html#ab12b4c1b5d0fa473ea78d3bd89131589", null ],
     [ "svc", "structdaos__pool__connect__t.html#a70e9cc69ae948a2a5fc72d8a60964400", null ],
-    [ "uuid", "structdaos__pool__connect__t.html#aad00e3514ef2dd4e2535dab4d84d0273", null ]
+    [ "uuid", "structdaos__pool__connect__t.html#a0c9cfc131b27ae3b98b1533bba4c58ef", null ]
 ];
